3f4ce6fd3059e827ee2ee50be73a5b875728dcc4
Ricky_Zeng <zengchiwai08@gmail.com>
2025-12-30 12:41:03 +0800

    Done some basic js and html

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello World !")
+const myLibrary = [];
+
+function Book(title, author, page, read){
+    if(!new.target){
+        throw Error("Please use the constructor with new keyword");
+    }
+    this.title = title;
+    this.author = author;
+    this.page = page;
+    this.read = read;
+    //window.crypto.randomUUID() , globalThis.crypto.randomUUID(), or crypto.randomUUID()
+    this.id = globalThis.crypto.randomUUID();
+}
+
+function addBookToLibrary(title,author,page,read){
+    let book = new Book(title,author,page,read);
+    myLibrary.push(book);
+}
